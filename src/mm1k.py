ac702c7c8739974249c8c3df90656cdab49e3e2b
mikiivanovic <mikiivanovic@icloud.com>
2021-05-28 00:46:09 +0100

    MM1k accepts parameters

--- src/mm1k.py
+++ src/mm1k.py
@@ -4,20 +4,18 @@ from numpy.random import exponential
 k = 20
 arrival_rate = 1
 service_rate = 2
-avg_arrival_time = 1 / arrival_rate
-avg_service_time = 1 / service_rate
 
 
 class MM1k(Gsmp):
     def states(self):
-        return range(k + 1)
+        return range(self._k + 1)
 
     def events(self):
         return ['arr', 'com']
 
     def e(self, s):
         es = []
-        if s < k:
+        if s < self._k:
             es.append('arr')
         if s > 0:
             es.append('com')
@@ -31,9 +29,9 @@ class MM1k(Gsmp):
 
     def f(self, _s, _e, s, e):
         if _e == 'arr':
-            return exponential(avg_arrival_time)
+            return exponential(self._avg_arrival_time)
         else:
-            return exponential(avg_service_time)
+            return exponential(self._avg_service_time)
 
     def r(self, s, e):
         return 1
@@ -42,11 +40,14 @@ class MM1k(Gsmp):
         return int(s == 0)
 
     def f_0(self, s, e):
-        return exponential(avg_arrival_time)
+        return exponential(self._avg_arrival_time)
 
     def __repr__(self):
         return str(self.name)
 
-    def __init__(self, name):
+    def __init__(self, name, _k=k, _arrival_rate=arrival_rate, _service_rate=service_rate):
         self.name = name
+        self._k = _k
+        self._avg_arrival_time = 1 / _arrival_rate
+        self._avg_service_time = 1 / _service_rate
         super().__init__()
